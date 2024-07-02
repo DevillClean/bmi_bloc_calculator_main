ff7e241528ba1f0fa15c15dcedf9156305d69753
DevillClean <sudziro62@gmail.com>
2024-07-02 21:18:50 +0300

    sign out

--- lib/presentation/screens/screen_1/widgets/bmi_menu_buttons.dart
+++ lib/presentation/screens/screen_1/widgets/bmi_menu_buttons.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_application_2/presentation/screens/screen_3/auth_screen.dart';
 import 'package:go_router/go_router.dart';
 
 class MenuButton extends StatefulWidget {
@@ -12,12 +13,17 @@ class MenuButton extends StatefulWidget {
 }
 
 class _MenuButtonState extends State<MenuButton> {
-  
   final user = FirebaseAuth.instance.currentUser!;
   void signOut() async {
     await FirebaseAuth.instance.signOut();
     // ignore: use_build_context_synchronously
-    Navigator.pop(context);
+    Navigator.push(
+        // ignore: use_build_context_synchronously
+        context,
+        MaterialPageRoute(
+            builder: (context) => AuthScreen(
+                  onTab: () {},
+                )));
   }
 
   @override
